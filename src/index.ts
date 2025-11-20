dbcb710facab92bb870a539f67a9e5f8427f72e2
TheLazySquid <thelazysquid2@gmail.com>
2025-11-19 21:02:21 -0500

    Make type optional when using workspaceConfig

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ export type * from "./types.js";
 import type { SingleConfig, WorkspaceConfig } from "./types.js";
 
 export function singleConfig(config: SingleConfig) { return config; }
-export function workspaceConfig(config: WorkspaceConfig) { return config; }
+
+type WorkspaceConfigTypeOptional = Omit<WorkspaceConfig, "type"> & { type?: "workspace" };
+export function workspaceConfig(config: WorkspaceConfigTypeOptional): WorkspaceConfig {
+    config.type = "workspace";
+    return config as WorkspaceConfig;
+}
